53cff76fc64c798ee66b4d4d8748b76988bab2c9
Kent Campbell <kentmcamp87@gmail.com>
2026-02-16 02:45:15 -0400

    updated app version

--- AppVersion.cs
+++ AppVersion.cs
@@ -8,13 +8,13 @@ public static class AppVersion
     // Single-source version numbers
     public const int Major = 1;
     public const int Minor = 0;
-    public const int Patch = 0;
+    public const int Patch = 1;
 
     public static string VersionString => $"{Major}.{Minor}.{Patch}";
 
     public static string DisplayName => $"v{VersionString}";
 
-    public static string Status => "Stable 1";
+    public static string Status => "Stable";
 
     public static string GetFullVersion() => $"{DisplayName} - {Status}";
 }
